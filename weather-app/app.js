50e77096c2833eb3a5d034c9c3bf593690eff687
BrunoAnjos <brunosilveiradosanjos@gmail.com>
2020-04-05 18:46:36 -0300

    6.33 An HTTP Request Challenge

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,12 +1,21 @@
-const key = ''
 const request = require('request')
-const sp_lat = -23.5489
-const sp_lon = -46.6388
+const city = 'Los Angeles'
+// const city = 'Belo Horizonte'
 
-url = 'http://api.openweathermap.org/data/2.5/weather?lat=' + sp_lat + '&lon=' + sp_lon + '&appid=' + key
-// url = 'https://api.openweathermap.org/data/2.5/find?q=Río&appid=' + key
+url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + city + '.json?access_token=' + mapBoxKey + '&limit=1'
 
-request({ url: url }, (error, response) => {
-    const data = JSON.parse(response.body)
-    console.log(data)
+// url = 'http://api.openweathermap.org/data/2.5/weather?units=metric&q=Dublin&appid=' + openWeatherKey + ''
+
+// request({ url: url, json: true }, (error, response) => {
+//     // console.log(url)
+//     // console.log(response.body.name)
+//     const message = 'it\'s currently ' + response.body.main.temp + ' degrees out.And it\'s ' + response.body.weather[0].description + ' in ' + response.body.name + '.'
+//     console.log(message)
+// })
+
+
+request({ url: url, json: true }, (error, response) => {
+    console.log(response.body.features[0].place_name)
+    const message = 'Place: ' + response.body.features[0].place_name + '. Latitude: ' + response.body.features[0].center[1] + ' and longitude: ' + response.body.features[0].center[0] + '.'
+    console.log(message)
 })
