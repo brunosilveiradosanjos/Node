223afc354dc290ce5438b2a09d61b2d33c114cd3
BrunoAnjos <brunosilveiradosanjos@gmail.com>
2020-04-09 11:23:15 -0300

    7.47 Dynamic Pages with Templating

--- web-app/src/app.js
+++ web-app/src/app.js
@@ -4,10 +4,28 @@ const express = require('express')
 const app = express()
 const publicDirectoryPath = path.join(__dirname, '../public')
 
+app.set('view engine', 'hbs')
 app.use(express.static(publicDirectoryPath))
 
 app.get('', (req, res) => {
-    res.send('<h1>Weather</h1>')
+    res.render('index', {
+        title: 'Weather app',
+        name: 'Andrew Mead'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Me',
+        name: 'Andrew Mead'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Help Me',
+        name: 'Andrew Mead'
+    })
 })
 
 app.get('/weather', (req, res) => {
