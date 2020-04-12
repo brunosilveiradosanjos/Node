0efaffae9df0ca8ff6306e2ac3ebeaefbc8c5a9d
BrunoAnjos <brunosilveiradosanjos@gmail.com>
2020-04-11 23:50:01 -0300

    8.58 Creating a Search Form

--- web-app/public/js/app.js
+++ web-app/public/js/app.js
@@ -1,19 +1,26 @@
-console.log('app.js was laded')
+const watherForm = document.querySelector('form')
+const search = document.querySelector('input')
+
+watherForm.addEventListener('submit', (event) => {
+    event.preventDefault()
+
+    const location = search.value
+    console.log('location: ' + location)
+
+    fetch('http://localhost:3000/weather?address=' + location).then((res) => {
+        res.json().then((data) => {
+            if (data.error) {
+                console.log(data.error)
+            } else {
+                console.log(data.location)
+                console.log(data.forecast)
+            }
+        })
+    })
+})
 
 // fetch('http://puzzle.mead.io/puzzle').then((res) => {
 //     res.json().then((data) => {
 //         console.log(data)
 //     })
 // })
-
-
-fetch('http://localhost:3000/weather?address=boston').then((res) => {
-    res.json().then((data) => {
-        if (data.error) {
-            console.log(data.error)
-        } else {
-            console.log(data.location)
-            console.log(data.forecast)
-        }
-    })
-})
