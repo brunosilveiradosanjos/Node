14d69de626d6ec925d792b7a1ca27941e6a8c20f
BrunoAnjos <brunosilveiradosanjos@gmail.com>
2020-04-11 23:29:38 -0300

    8.57 Browser HTTP Requests with Fetch

--- web-app/public/js/app.js
+++ web-app/public/js/app.js
@@ -1,1 +1,19 @@
 console.log('app.js was laded')
+
+// fetch('http://puzzle.mead.io/puzzle').then((res) => {
+//     res.json().then((data) => {
+//         console.log(data)
+//     })
+// })
+
+
+fetch('http://localhost:3000/weather?address=boston').then((res) => {
+    res.json().then((data) => {
+        if (data.error) {
+            console.log(data.error)
+        } else {
+            console.log(data.location)
+            console.log(data.forecast)
+        }
+    })
+})
