ab2746fd6e72c06dc29e35be77210948d5170e31
BrunoAnjos <brunosilveiradosanjos@gmail.com>
2020-04-17 19:05:37 -0300

    10.80 Updating Documents

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -12,30 +12,14 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
 
     const db = client.db(databaseName)
 
-    db.collection('tasks').findOne({ _id: new ObjectID("5e990eab3bd36e3b6a095f46") }, (error, task) => {
-        if (error) {
-            return console.log('Culdnt find taks')
+    db.collection('tasks').updateMany({}, {
+        $set: {
+            completed: true
         }
-        console.log(task)
+    }).then((result) => {
+        console.log(result.modifiedCount)
+    }).catch((error) => {
+        console.log(error)
     })
 
-    db.collection('tasks').find({ completed: true }).toArray((error, tasks) => {
-
-        console.log(tasks)
-    })
-
-    // db.collection('users').findOne({ _id: new ObjectID("5e99002814627b331c20d59e") }, (error, user) => {
-    //     if (error) {
-    //         return ('error')
-    //     }
-    //     console.log(user)
-    // })
-
-    // db.collection('users').find({ age: 27 }).toArray((error, users) => {
-    //     console.log(users)
-    // })
-
-    // db.collection('users').find({ age: 27 }).count((error, count) => {
-    //     console.log(count)
-    // })
 })
