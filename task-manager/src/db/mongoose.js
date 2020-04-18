332ffa623687944e79ecdb8483bd73b40ef46ab3
BrunoAnjos <brunosilveiradosanjos@gmail.com>
2020-04-18 00:57:39 -0300

    11.85 Data Validation and Sanitization: Part I

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const validator = require('validator')
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useNewUrlParser: true,
@@ -6,42 +7,61 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
 })
 
 
-const Task = mongoose.model('Task', {
-    description: {
-        type: String
-    },
-    completed: {
-        type: Boolean
-    }
-})
-
-const task = new Task({
-    description: 'Sleep',
-    completed: false
-})
-
-task.save().then((tast) => {
-    console.log(task)
-}).catch((error) => {
-    console.log(error)
-})
-
-// const User = mongoose.model('User', {
-//     name: {
+// const Task = mongoose.model('Task', {
+//     description: {
 //         type: String
 //     },
-//     age: {
-//         type: Number
+//     completed: {
+//         type: Boolean
 //     }
 // })
 
-// const me = new User({
-//     name: 'Bruno',
-//     age: 28
+// const task = new Task({
+//     description: 'Sleep',
+//     completed: false
 // })
 
-// me.save().then((me) => {
-//     console.log(me)
+// task.save().then((tast) => {
+//     console.log(task)
 // }).catch((error) => {
 //     console.log(error)
 // })
+
+const User = mongoose.model('User', {
+    name: {
+        type: String,
+        require: true
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error('e-mail is not valid')
+            }
+        }
+    },
+    age: {
+        type: Number,
+        default: 0,
+        validate(value) {
+            if (value < 0) {
+                throw new Error('Age must be greater than zero')
+            }
+        }
+    }
+})
+
+const me = new User({
+    name: 'Giovana',
+    email: 'giovana@gmail.com',
+    age: 30
+})
+
+me.save().then((me) => {
+    console.log(me)
+}).catch((error) => {
+    console.log(error)
+})
