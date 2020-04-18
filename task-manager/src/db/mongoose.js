e7b2702d2a6d5544e56588c7011da895c2186e9f
BrunoAnjos <brunosilveiradosanjos@gmail.com>
2020-04-18 01:45:21 -0300

    11.86 Data Validation and Sanitization: Part II

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -7,61 +7,76 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
 })
 
 
-// const Task = mongoose.model('Task', {
-//     description: {
-//         type: String
-//     },
-//     completed: {
-//         type: Boolean
-//     }
-// })
-
-// const task = new Task({
-//     description: 'Sleep',
-//     completed: false
-// })
-
-// task.save().then((tast) => {
-//     console.log(task)
-// }).catch((error) => {
-//     console.log(error)
-// })
-
-const User = mongoose.model('User', {
-    name: {
+const Task = mongoose.model('Task', {
+    description: {
         type: String,
-        require: true
-    },
-    email: {
-        type: String,
-        required: true,
         trim: true,
-        lowercase: true,
-        validate(value) {
-            if (!validator.isEmail(value)) {
-                throw new Error('e-mail is not valid')
-            }
-        }
+        required: true
     },
-    age: {
-        type: Number,
-        default: 0,
-        validate(value) {
-            if (value < 0) {
-                throw new Error('Age must be greater than zero')
-            }
-        }
+    completed: {
+        type: Boolean,
+        default: false
     }
 })
 
-const me = new User({
-    name: 'Giovana',
-    email: 'giovana@gmail.com',
-    age: 30
+const task = new Task({
+    description: 'Sleep',
+    completed: false
 })
 
-me.save().then((me) => {
-    console.log(me)
+task.save().then((tast) => {
+    console.log(task)
 }).catch((error) => {
     console.log(error)
 })
+
+// const User = mongoose.model('User', {
+//     name: {
+//         type: String,
+//         require: true
+//     },
+//     email: {
+//         type: String,
+//         required: true,
+//         trim: true,
+//         lowercase: true,
+//         validate(value) {
+//             if (!validator.isEmail(value)) {
+//                 throw new Error('e-mail is not valid')
+//             }
+//         }
+//     },
+//     password: {
+//         type: String,
+//         required: true,
+//         minlength: 7,
+//         trim: true,
+//         validate(value) {
+//             if (value.toLowerCase().includes('password')) {
+//                 throw new Error('Password cannot contain the word password')
+//             }
+//         }
+//     },
+//     age: {
+//         type: Number,
+//         default: 0,
+//         validate(value) {
+//             if (value < 0) {
+//                 throw new Error('Age must be greater than zero')
+//             }
+//         }
+//     }
+// })
+
+// const me = new User({
+//     name: 'Giovana',
+//     email: 'giovana@gmail.com',
+//     password: 'oaidva[oinvd[aod083',
+//     age: 30
+// })
+
+// me.save().then((me) => {
+//     console.log(me)
+// }).catch((error) => {
+//     console.log(error)
+// })
