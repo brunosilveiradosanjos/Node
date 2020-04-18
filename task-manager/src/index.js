bf0fd82e7872230bbba0a7b9d51fd3603669f936
BrunoAnjos <brunosilveiradosanjos@gmail.com>
2020-04-18 18:11:13 -0300

    11.92 Resource Reading Endpoints: Part II

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -23,7 +23,7 @@ app.get('/users', (req, res) => {
     User.find({}).then((users) => {
         res.send(users)
     }).catch((e) => {
-        res.status(201).send()
+        res.status(500).send()
     })
 })
 
@@ -35,17 +35,36 @@ app.get('/users/:id', (req, res) => {
         }
         res.send(user)
     }).catch((e) => {
-        res.status(404).send()
+        res.status(500).send(e)
     })
 })
 
 app.post('/tasks', (req, res) => {
-    // console.log(req.body)
     const task = new Task(req.body)
     task.save().then(() => {
         res.status(201).send(task)
     }).catch((e) => {
         res.status(400).send(e)
+    })
+})
+
+app.get('/tasks', (req, res) => {
+    Task.find({}).then((tasks) => {
+        res.send(tasks)
+    }).catch((e) => {
+        res.status(500).send(e)
+    })
+})
+
+app.get('/tasks/:id', (req, res) => {
+    const _id = req.params.id
+    Task.findById(_id).then((task) => {
+        if (!task) {
+            return res.status(404).send()
+        }
+        res.send(task)
+    }).catch((e) => {
+        res.status(500).send(e)
     })
 })
 
