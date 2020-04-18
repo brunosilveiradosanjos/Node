42cc1dc6944467034db1e353d37ca3dd6367c31f
BrunoAnjos <brunosilveiradosanjos@gmail.com>
2020-04-18 16:49:13 -0300

    11.90 Resource Creation Endpoints: Part II

---
+++ task-manager/src/models/task.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+const validadtor = require('validator')
+
+const Task = mongoose.model('Task', {
+    description: {
+        type: String,
+        required: true
+    },
+    completed: {
+        type: Boolean,
+        default: false
+    }
+})
+
+module.exports = Task
