3eccc7e3c6ec4181c7e8bc3bfccb32835c813a69
BrunoAnjos <brunosilveiradosanjos@gmail.com>
2020-04-18 16:20:56 -0300

    11.89 Resource Creation Endpoints: Part I

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose')
-const validator = require('validator')
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useNewUrlParser: true,
@@ -7,65 +6,27 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
 })
 
 
-const Task = mongoose.model('Task', {
-    description: {
-        type: String,
-        trim: true,
-        required: true
-    },
-    completed: {
-        type: Boolean,
-        default: false
-    }
-})
-
-const task = new Task({
-    description: 'Sleep',
-    completed: false
-})
-
-task.save().then((tast) => {
-    console.log(task)
-}).catch((error) => {
-    console.log(error)
-})
-
-// const User = mongoose.model('User', {
-//     name: {
+// const Task = mongoose.model('Task', {
+//     description: {
 //         type: String,
-//         require: true
-//     },
-//     email: {
-//         type: String,
-//         required: true,
 //         trim: true,
-//         lowercase: true,
-//         validate(value) {
-//             if (!validator.isEmail(value)) {
-//                 throw new Error('e-mail is not valid')
-//             }
-//         }
+//         required: true
 //     },
-//     password: {
-//         type: String,
-//         required: true,
-//         minlength: 7,
-//         trim: true,
-//         validate(value) {
-//             if (value.toLowerCase().includes('password')) {
-//                 throw new Error('Password cannot contain the word password')
-//             }
-//         }
-//     },
-//     age: {
-//         type: Number,
-//         default: 0,
-//         validate(value) {
-//             if (value < 0) {
-//                 throw new Error('Age must be greater than zero')
-//             }
-//         }
+//     completed: {
+//         type: Boolean,
+//         default: false
 //     }
+// })
+
+// const task = new Task({
+//     description: 'Sleep',
+//     completed: false
+// })
+
+// task.save().then((tast) => {
+//     console.log(task)
+// }).catch((error) => {
+//     console.log(error)
 // })
 
 // const me = new User({
