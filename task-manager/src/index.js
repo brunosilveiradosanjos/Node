dbce6fd04b003acdbb8987452436de965a47a14d
BrunoAnjos <brunosilveiradosanjos@gmail.com>
2020-04-20 17:10:30 -0300

    11.100 Resource Deleting Endpoints

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -132,6 +132,35 @@ app.patch('/tasks/:id', async (req, res) => {
     }
 })
 
+/***********************************  
+************DELETE by ID************
+************************************/
+
+app.delete('/users/:id', async (req, res) => {
+    try {
+        const user = await User.findByIdAndDelete(req.params.id)
+        console.log(user)
+        if (!user) {
+            return res.status(404).send()
+        }
+        res.send(user)
+    } catch (e) {
+        res.status(500).send()
+    }
+})
+
+app.delete('/tasks/:id', async (req, res) => {
+    try {
+        const task = await Task.findByIdAndDelete(req.params.id)
+        if (!task) {
+            return res.status(404).send()
+        }
+        res.send(task)
+    } catch (e) {
+        res.status(500).send()
+    }
+})
+
 app.listen(port, () => {
     console.log('Port is up on p ort ' + port)
 })
